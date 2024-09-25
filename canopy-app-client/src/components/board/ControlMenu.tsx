83e776ae7f2651b158efff0585254de2a9e084b4
Jisu0917 <bjisu8099@gmail.com>
2024-09-25 16:08:43 +0900

    feat: Color control label based on the checkbox state

--- canopy-app-client/src/components/board/ControlMenu.tsx
+++ canopy-app-client/src/components/board/ControlMenu.tsx
@@ -28,15 +28,15 @@ const ControlMenu: React.FC<ControlMenuProps> = ({
   const controls = [
     {
       name: "fold",
-      label: "그늘막 펴기/접기",
+      label: "수동 펼침 / 접힘",
       status: localStatus.status_fold,
     },
-    { name: "motor", label: "모터 작동", status: localStatus.status_motor },
-    { name: "led", label: "LED 작동", status: localStatus.status_led },
-    { name: "sound", label: "스피커 작동", status: localStatus.status_sound },
+    { name: "motor", label: "모터 정지", status: localStatus.status_motor },
+    { name: "led", label: "조명", status: localStatus.status_led },
+    { name: "sound", label: "사운드", status: localStatus.status_sound },
     {
       name: "inform",
-      label: "안내멘트 작동",
+      label: "정보갱신",
       status: localStatus.status_inform,
     },
   ];
@@ -64,10 +64,17 @@ const ControlMenu: React.FC<ControlMenuProps> = ({
   };
 
   return (
-    <div className="bg-transparent py-2 pl-40 pr-8 space-y-2">
+    <div className="bg-transparent pb-3 pl-40 pr-8 space-y-3">
       {controls.map((control) => (
-        <div key={control.name} className="flex justify-between items-center">
-          <span className="text-gray-500">{control.label}</span>
+        <div
+          key={control.name}
+          className="flex justify-between items-center h-7"
+        >
+          <span
+            className={`${control.status ? "text-gray-800" : "text-gray-400"}`}
+          >
+            {control.label}
+          </span>
           {loading[control.name] ? (
             <Image
               src={Loading.src}
@@ -84,7 +91,7 @@ const ControlMenu: React.FC<ControlMenuProps> = ({
                 checked={control.status ?? false}
                 onChange={() => handleToggle(control.name, control.status)}
               />
-              <div className="w-11 h-6 bg-gray-100 peer-focus:outline-gray-200 peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-[#4dc1e9] rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#4dc1e9]"></div>
+              <div className="w-11 h-6 bg-gray-200 peer-focus:outline-gray-200 peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-[#4dc1e9] rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#4dc1e9]"></div>
             </label>
           )}
         </div>
